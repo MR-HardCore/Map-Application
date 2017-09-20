72a86e694b6451258505b6f1d7d98197b2e85bc7
Jixiong Deng <jasondengjx@gmail.com>
2017-09-19 23:28:25 -0400

    Final Edition
    Integrated various features like finding shortest path and provided an optimized solution for Traveling Salesman Problem.
    Code should be compiled under Linux.

---
+++ .svn/pristine/e6/e6a83f429fa8b0d2aa38d3d91ba4a2b81732146a.svn-base
@@ -0,0 +1,13 @@
+#include "StreetsDatabaseAPI.h"
+#include "m1.h"
+#include "m2.h"
+#include "m3.h"
+
+using namespace std;
+
+int main() {
+
+    load_milestone3();
+
+    return 0;
+}
---
+++ build/release/libstreetmap/src/structure.d
@@ -0,0 +1,38 @@
+build/release/libstreetmap/src/structure.o: \
+ libstreetmap/src/structure.cpp libstreetmap/src/structure.h \
+ libstreetsdatabase/src/StreetsDatabaseAPI.h \
+ libstreetsdatabase/src/LatLon.h libstreetsdatabase/src/Feature.h \
+ libstreetsdatabase/src/OSMEntityType.h \
+ libstreetsdatabase/src/OSMDatabaseAPI.h \
+ libstreetsdatabase/src/OSMEntity.hpp libstreetsdatabase/src/OSMNode.hpp \
+ libstreetsdatabase/src/OSMWay.hpp libstreetsdatabase/src/OSMRelation.hpp \
+ libstreetmap/src/graphics.h libstreetmap/src/easygl_constants.h \
+ libstreetmap/src/m1.h libstreetmap/src/m2.h
+
+libstreetmap/src/structure.h:
+
+libstreetsdatabase/src/StreetsDatabaseAPI.h:
+
+libstreetsdatabase/src/LatLon.h:
+
+libstreetsdatabase/src/Feature.h:
+
+libstreetsdatabase/src/OSMEntityType.h:
+
+libstreetsdatabase/src/OSMDatabaseAPI.h:
+
+libstreetsdatabase/src/OSMEntity.hpp:
+
+libstreetsdatabase/src/OSMNode.hpp:
+
+libstreetsdatabase/src/OSMWay.hpp:
+
+libstreetsdatabase/src/OSMRelation.hpp:
+
+libstreetmap/src/graphics.h:
+
+libstreetmap/src/easygl_constants.h:
+
+libstreetmap/src/m1.h:
+
+libstreetmap/src/m2.h:
